c6ec1b4f9f80a6ca4be8b74252c39575a815412a
devjaymmy <james.nganga@student.moringaschool.com>
2022-01-18 23:30:29 +0300

    added order buttons below crust images

--- js/script.js
+++ js/script.js
@@ -2,21 +2,14 @@ $(document).ready(function () {
   $("#img1").hover(function () {
     $(".crispy-crust").toggle();
   });
-});
-
-$(document).ready(function () {
   $("#img2").hover(function () {
     $(".stuffed-crust").toggle();
   });
-});
-$(document).ready(function () {
   $("#img3").hover(function () {
     $(".gluttenFree-crust").toggle();
   });
-});
-
-$(document).ready(function () {
   $("#img4").hover(function () {
     $(".cracker-crust").toggle();
   });
+  Event.preventDefault();
 });
