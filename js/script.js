99c2a12865dc6c635a08c01ff5629630600d3afd
devjaymmy <james.nganga@student.moringaschool.com>
2022-01-18 23:04:57 +0300

    added other crust types together with overlay

--- js/script.js
+++ js/script.js
@@ -3,3 +3,20 @@ $(document).ready(function () {
     $(".crispy-crust").toggle();
   });
 });
+
+$(document).ready(function () {
+  $("#img2").hover(function () {
+    $(".stuffed-crust").toggle();
+  });
+});
+$(document).ready(function () {
+  $("#img3").hover(function () {
+    $(".gluttenFree-crust").toggle();
+  });
+});
+
+$(document).ready(function () {
+  $("#img4").hover(function () {
+    $(".cracker-crust").toggle();
+  });
+});
